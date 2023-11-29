dbc2381ef472f7ef915ca239f566e8d9516603ac
wiglejs <jamesbusiness43@gmail.com>
2023-11-28 21:17:43 -0700

    Add project files.

---
+++ StudentScores/frmStudentScores.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace StudentScores
+{
+    partial class frmStudentScores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmStudentScores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 208);
+            this.Font = new System.Drawing.Font("Microsoft Uighur", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "frmStudentScores";
+            this.Text = "Student Scores";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
